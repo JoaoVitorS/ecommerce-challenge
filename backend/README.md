f54cc443e20664875ca39d234add03a751ad63de
João Vitor <joao_vitorsoares@hotmail.com>
2025-10-30 19:36:01 -0300

    chore: initial project setup

---
+++ backend/README.md
@@ -0,0 +1,16 @@
+# 🧩 Backend - Desafio Técnico
+
+API mock em Node.js + Express + TypeScript para desafio técnico Stefanini - Ecommerce
+
+### 🔗 Endpoints
+| Método | Rota | Descrição |
+|--------|------|------------|
+| GET | /api/products/:id | Retorna dados detalhados de um produto |
+| GET | /api/cart | Retorna todos os itens que estão presentes no carrinho |
+| POST | /api/cart/add | Adiciona ou remove produto |
+
+### ▶️ Como rodar
+```bash
+cd backend
+npm install
+npm run dev
---
+++ backend/src/server.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import productsRoutes from "./routes/products";
+import cartRoutes from "./routes/cart";
+import "dotenv/config"
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_req, res) => res.json({ ok: true }));
+
+app.use("/api/products", productsRoutes);
+app.use("/api/cart", cartRoutes);
+
+app.use((_req, res) => res.status(404).json({ message: "Not found" }));
+
+app.use((err: any, _req: any, res: any, _next: any) => {
+  console.error(err);
+  res.status(500).json({ message: "Internal server error" });
+});
+
+const PORT = Number(4000);
+const HOST = process.env.HOST ?? "0.0.0.0";
+
+const server = app.listen(PORT, HOST, () => {
+  console.log("✅ API Rodando:", server.address());
+});
+
+server.on("error", (err: any) => {
+  console.error("❌ Erro no listen:", err.code, err.message);
+});
