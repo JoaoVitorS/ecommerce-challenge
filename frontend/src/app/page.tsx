c950928ab00808c34de0cf76c2954d33b691b871
João Vitor <joao_vitorsoares@hotmail.com>
2025-10-30 21:44:45 -0300

    fix: some fixes product page and products info

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -17,7 +17,7 @@ export default async function HomePage({ searchParams }: Props) {
 
   return (
     <>
-      <p className="mb-4 text-gray-600">Periféricos</p>
+      <p className="mb-4 text-gray-600">Periféricos Eletrônicos</p>
       <section className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
         {products.map((p) => (
           <ProductCard key={p.id} product={p} />
--- frontend/src/app/product/[id]/page.tsx
+++ frontend/src/app/product/[id]/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image"
+import { Info } from "lucide-react";
 import { apiGet } from "@/lib/api";
 import type { Product } from "@/lib/types";
 import ProductPrice from "@/components/ProductPrice";
@@ -23,12 +23,14 @@ export default async function ProductPage({ params }: Props) {
         <div className="flex flex-col justify-center space-y-4">
           <h1 className="text-3xl font-semibold font-color text-(--color-base)">{product.name}</h1>
           <ProductPrice product={product} />
-
+          <div className="flex items-center space-x-2">
+            <Info className="items-center h-4 w-4 text-[#002E5C]" aria-placeholder="Informativo do produto"/>
+            <h3 className="text-1xl font-semibold font-color text-[#002E5C]">Sobre o produto</h3>
+          </div>
           <p className="text-gray-700 leading-relaxed">
             {product.description ||
               "Descrição indisponível. Este produto ainda não possui detalhes cadastrados."}
           </p>
-
           <div className="pt-4">
             <AddToCartButton productId={product.id} />
           </div>
